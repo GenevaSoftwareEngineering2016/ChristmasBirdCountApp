e9f6b5945222084976c533c18aea9e8d3f28633a
Austin Caldwell <austin.caldwell295@gmail.com>
2016-10-04 16:19:46 -0400

    First commit to new official app repo.  Set basic app settings related to release (debugging and architecture sets).  No changes to app GUI or functionality have yet been made.

---
+++ ChristmasBirdCountApp/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿// (c) 2016 Geneva College Senior Software Project Team
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ChristmasBirdCountApp
+{
+    [Activity(Label = "ChristmasBirdCountApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ ChristmasBirdCountApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChristmasBirdCountApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChristmasBirdCountApp")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Automatically enable or disable debugging depending on whether the application is set to "DEBUG" state
+// "Release" applications should NOT enable debugging (security risk)
+#if DEBUG
+[assembly: Application(Debuggable = true)]
+#else
+[assembly: Application(Debuggable=false)]
+#endif
