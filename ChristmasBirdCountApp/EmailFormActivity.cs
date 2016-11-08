64588628cc8bd9b2853a5c4795fe6ff2e1c7b435
Austin Caldwell <austin.caldwell295@gmail.com>
2016-11-07 21:42:18 -0500

    Added variables for all screen fields and button.  Added event handlers for all fields and buttons except for 'send' button.  Still need to gather information from fields and save information appropriately.

--- ChristmasBirdCountApp/EmailFormActivity.cs
+++ ChristmasBirdCountApp/EmailFormActivity.cs
@@ -2,6 +2,7 @@ using System;
 using Android.App;
 using Android.Content;
 using Android.OS;
+using Android.Text;
 using Android.Widget;
 
 namespace ChristmasBirdCountApp
@@ -9,34 +10,125 @@ namespace ChristmasBirdCountApp
     [Activity(Label = "Bird Counter", Icon = "@drawable/audubon_society2")]
     public class EmailFormActivity : Activity
     {
+        private EditText _recipientEmail;
+        private Spinner _regionSelection;
+        private EditText _hoursDriven;
+        private EditText _milesDriven;
+        private EditText _hoursWalked;
+        private EditText _milesWalked;
+        private EditText _hoursOwling;
+        private EditText _partyMembers;
+        private EditText _optionalNotes;
+        private Button _returnButton;
+        private Button _sendButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.EmailForm);
-            Button btnReturn = FindViewById<Button>(Resource.Id.btnReturn);
-            btnReturn.Click += BtnReturn_Click;
 
-            // Create your application here
-        }
-
-        private void BtnReturn_Click(object sender, EventArgs e)
-        {
-            var intent = new Intent(this, typeof(MainActivity));
-            StartActivity(intent);
-
-            SetContentView(Resource.Layout.Main);
+            // Register Fields and Buttons
+            _recipientEmail = FindViewById<EditText>(Resource.Id.txtRecipientEmail);
+            _regionSelection = FindViewById<Spinner>(Resource.Id.spnRegion);
+            _hoursDriven = FindViewById<EditText>(Resource.Id.txtHoursDriven);
+            _milesDriven = FindViewById<EditText>(Resource.Id.txtMilesDriven);
+            _hoursWalked = FindViewById<EditText>(Resource.Id.txtHoursWalked);
+            _milesWalked = FindViewById<EditText>(Resource.Id.txtMilesWalked);
+            _hoursOwling = FindViewById<EditText>(Resource.Id.txtHoursOwling);
+            _partyMembers = FindViewById<EditText>(Resource.Id.txtPartyMembers);
+            _optionalNotes = FindViewById<EditText>(Resource.Id.txtNotes);
+            _returnButton = FindViewById<Button>(Resource.Id.btnReturn);
+            _sendButton = FindViewById<Button>(Resource.Id.btnSend);
         }
 
         protected override void OnStart()
         {
             base.OnStart();
+
             // Register event handlers
+            _recipientEmail.TextChanged += RecipientEmailOnTextChanged;
+            _regionSelection.ItemSelected += RegionSelectionOnItemSelected;
+            _hoursDriven.TextChanged += HoursDrivenOnTextChanged;
+            _milesDriven.TextChanged += MilesDrivenOnTextChanged;
+            _hoursWalked.TextChanged += HoursWalkedOnTextChanged;
+            _milesWalked.TextChanged += MilesWalkedOnTextChanged;
+            _hoursOwling.TextChanged += HoursOwlingOnTextChanged;
+            _partyMembers.TextChanged += PartyMembersOnTextChanged;
+            _optionalNotes.TextChanged += OptionalNotesOnTextChanged;
+            _returnButton.Click += ReturnButtonOnClick;
+            // Send Button Event Handler
+
         }
 
         protected override void OnStop()
         {
             // Deregister event handlers
+            _recipientEmail.TextChanged -= RecipientEmailOnTextChanged;
+            _regionSelection.ItemSelected-= RegionSelectionOnItemSelected;
+            _hoursDriven.TextChanged -= HoursDrivenOnTextChanged;
+            _milesDriven.TextChanged -= MilesDrivenOnTextChanged;
+            _hoursWalked.TextChanged -= HoursWalkedOnTextChanged;
+            _milesWalked.TextChanged -= MilesWalkedOnTextChanged;
+            _hoursOwling.TextChanged -= HoursOwlingOnTextChanged;
+            _partyMembers.TextChanged -= PartyMembersOnTextChanged;
+            _optionalNotes.TextChanged -= OptionalNotesOnTextChanged;
+            _returnButton.Click -= ReturnButtonOnClick;
+            // Send Button Event Handler
+
             base.OnStop();
+        }
+
+        private void RecipientEmailOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegionSelectionOnItemSelected(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HoursDrivenOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MilesDrivenOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HoursWalkedOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MilesWalkedOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HoursOwlingOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PartyMembersOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OptionalNotesOnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ReturnButtonOnClick(object sender, EventArgs e)
+        {
+            // Return to the Main App Screen
+            var intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+            SetContentView(Resource.Layout.Main);
         }
     }
 }
