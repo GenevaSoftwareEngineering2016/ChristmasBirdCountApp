8b49baf7f8c5279653fbcb4f37a0f79268673232
Austin Caldwell <austin.caldwell295@gmail.com>
2016-11-15 17:39:49 -0500

    Added new class to decrypt email password (stored as encrypted password).  Borrowed code from StackOverflow post (see class code).  Not currently working - the app fails when you go to send the email.  Probably becuase the string for password is not being found.

--- ChristmasBirdCountApp/EmailService/SmtpConnection.cs
+++ ChristmasBirdCountApp/EmailService/SmtpConnection.cs
@@ -1,18 +1,34 @@
 ﻿// (c) 2016 Geneva College Senior Software Project Team
 using System;
-using System.Runtime.Versioning;
+using System.Reflection;
+using System.Resources;
 using MailKit.Net.Smtp;
 
 namespace EmailService
 {
     public class SmtpConnection
     {
+        private string _encryptedPassword;
+        private string _sharedSecret;
+        private string _emailPassword;
+
         public SmtpClient Client { get; set; }
 
         public void CreateSmtpConnection()
         {
             // Set Up SMTP Client
             Client = new SmtpClient();
+
+            // Decrypt Email Password
+
+            // 1) Get Encrypted Password
+            Assembly assembly = this.GetType().Assembly;
+            ResourceManager resourceManager = new ResourceManager("Resources.EmailStrings", assembly);
+            _encryptedPassword = resourceManager.GetString("EmailPassword");
+            _sharedSecret = resourceManager.GetString("SharedSecret");
+
+            // 2) Decrypt Password
+            _emailPassword = Decryptor.DecryptStringAES(_encryptedPassword, _sharedSecret);
 
             try
             {
@@ -25,7 +41,7 @@ namespace EmailService
                 Client.AuthenticationMechanisms.Remove("XOAUTH2");
 
                 // SMTP Server Requires Authentication
-                Client.Authenticate("gc.seniorsoftwareproject@gmail.com", "-----");
+                Client.Authenticate("gc.seniorsoftwareproject@gmail.com", _emailPassword);
             }
             catch (Exception)
             {
